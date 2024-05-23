43280fb0023fa46db8d69294ac0979e6dcfb3c1c
Aymen Bouazizi <aymendotbouazizi@gmail.com>
2024-05-23 14:49:35 +0100

    Extract task creation form to own file

--- task-manager-frontend/src/App.js
+++ task-manager-frontend/src/App.js
@@ -1,55 +1,11 @@
-import { useState } from "react";
-import axios from "axios";
 import "./App.css";
+import TaskForm from "./components/TaskForm";
 
 function App() {
-  const [task, setTask] = useState("");
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!task.trim()) {
-      alert("Task cannot be empty!");
-    } else {
-      console.log(task);
-      postData();
-      setTask("");
-    }
-  };
-  const handleChange = (e) => {
-    setTask(e.target.value);
-  };
-
-  const postData = () => {
-    const apiUrl = `http://localhost:8080/api/v1/taskmanager`;
-    axios
-      .post(apiUrl, { name: task })
-      .then((res) => {
-        console.log(res.data); // Use res.data to access the actual data
-      })
-      .catch((e) => {
-        console.log("Error!");
-        console.error(e); // Handle errors here
-      });
-  };
-
   return (
     <div className="App">
       <h1>Task Manager</h1>
-      <div className="task-container">
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="task">Task</label>
-          <input
-            type="text"
-            id="task"
-            name="task"
-            placeholder="Enter your task"
-            value={task}
-            onChange={handleChange}
-          />
-          <button type="submit" onSubmit={handleSubmit}>
-            Submit
-          </button>
-        </form>
-      </div>
+      <TaskForm />
     </div>
   );
 }
